6323cf5bad102d6e36a055fc56bba623b50705a7
Lokendra <lokilokendra12345@gmail.com>
2020-11-01 23:02:47 +0530

    [lokendra] added extra UC

--- AdressBookDbms.sql
+++ AdressBookDbms.sql
@@ -31,7 +31,7 @@ insert into Address_Book values('lokendra','yeddula','tirupati',
 'TPT','AP',542593,'+91 7085237649','lokendra@gmail.com');
 
 
---UC4 Edit the existing contact using FirstName
+--UC4 Edit the existing contact using lastName
 
 update Address_Book set email='saurav123@gmail.com' where firstname='saurab';
 update Address_Book set lastname='moturi' where lastname='yeddula';
@@ -57,7 +57,7 @@ select city,count(*) as NoOfPersons from Address_Book group by city;
 
 --UC8 get the city wise sorted contacts
 
-select * from Address_Book  where State='MR' order by firstname ;
+select * from Address_Book  where city='MUM' order by firstname ;
 
 select * from Address_Book;
 
@@ -86,3 +86,24 @@ insert into Address_Book values('gunjan','sharma','delhi',
 'delhi','DLH',533293,'+91 9876543210','gunjan@gmail.com','gunjan','Family');
 
 select * from Address_Book;
+
+--UC12 create a Contact_Type table to remove redundancy
+create table Contact_Type 
+(firstname varchar(20) ,
+Type varchar(20) primary key(firstname,Type)
+);
+
+insert into Contact_Type values('karan','brother');
+insert into Contact_Type values('ajay','Friend');
+insert into Contact_Type values('vijay','Friends');
+insert into Contact_Type values('saurabh','Family');
+insert into Contact_Type values('lokendra','myself');
+insert into Contact_Type values('karan','Family');
+drop table Contact_Type;
+
+
+select * from Contact_Type;
+
+--UC13 Ensure all retrive queries are working fine
+
+select AB.firstname,AB.lastname ,b.Type from Address_Book AB inner join Contact_Type CT on AB.firstname=CT.firstname ; 
